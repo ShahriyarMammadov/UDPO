c7b118b24a749a1ef572948be0adebc74f240571
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-09-23 22:00:09 +0400

    alice carousel

--- src/components/collegausCarousel/index.jsx
+++ src/components/collegausCarousel/index.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./index.scss";
-// import genclerveidmannaziri from "../../assets/images/collegaus/genclerveidmannazirliyi.jpg";
-// import ecosoc from "../../assets/images/collegaus/ecosoc.jpg";
-// import insanhuquqlari from "../../assets/images/collegaus/insanhuquqlari.jpg";
-// import medeniyyetveturizmnazirliyi from "../../assets/images/collegaus/medeniyyetveturizmnazirliyi.png";
-// import ombudsman from "../../assets/images/collegaus/ombudsman.png";
-// import sehersalma from "../../assets/images/collegaus/sehersalma.jpg";
-// import sosialmudafienazirliyi from "../../assets/images/collegaus/sosialmudafienazirliyi.png";
-// import undesa from "../../assets/images/collegaus/undesa.jpg";
-// import undp from "../../assets/images/collegaus/undp.png";
+import genclerveidmannaziri from "../../assets/images/collegaus/genclerveidmannazirliyi.jpg";
+import ecosoc from "../../assets/images/collegaus/ecosoc.jpg";
+import insanhuquqlari from "../../assets/images/collegaus/insanhuquqlari.jpg";
+import medeniyyetveturizmnazirliyi from "../../assets/images/collegaus/medeniyyetveturizmnazirliyi.png";
+import ombudsman from "../../assets/images/collegaus/ombudsman.png";
+import sehersalma from "../../assets/images/collegaus/sehersalma.jpg";
+import sosialmudafienazirliyi from "../../assets/images/collegaus/sosialmudafienazirliyi.png";
+import undesa from "../../assets/images/collegaus/undesa.jpg";
+import undp from "../../assets/images/collegaus/undp.png";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { Image } from "antd";
@@ -22,48 +22,43 @@ const ColleaguesComponent = () => {
   };
 
   const items = [
-    // <div className="item" data-value="1">
-    //   <Image src={genclerveidmannaziri} alt="gencler ve idman nazirliyi" />
-    // </div>,
-    // <div className="item" data-value="2">
-    //   <Image src={ecosoc} alt="ecosoc" />
-    // </div>,
-    // <div className="item" data-value="3">
-    //   <Image src={insanhuquqlari} alt="insanhuquqlari" />
-    // </div>,
-    // <div className="item" data-value="4">
-    //   <Image
-    //     src={medeniyyetveturizmnazirliyi}
-    //     alt="medeniyyetveturizmnazirliyi"
-    //   />
-    // </div>,
-    // <div className="item" data-value="5">
-    //   <Image src={ombudsman} alt="ombudsman" />
-    // </div>,
-    // <div className="item" data-value="6">
-    //   <Image src={sehersalma} alt="sehersalma" />
-    // </div>,
-    // <div className="item" data-value="7">
-    //   <Image src={sosialmudafienazirliyi} alt="sosialmudafienazirliyi" />
-    // </div>,
-    // <div className="item" data-value="8">
-    //   <Image src={undesa} alt="undesa" />
-    // </div>,
-    // <div className="item" data-value="9">
-    //   <Image src={undp} alt="undp" />
-    // </div>,
+    <div className="item" data-value="1">
+      <Image src={genclerveidmannaziri} alt="gencler ve idman nazirliyi" />
+    </div>,
+    <div className="item" data-value="2">
+      <Image src={ecosoc} alt="ecosoc" />
+    </div>,
+    <div className="item" data-value="3">
+      <Image src={insanhuquqlari} alt="insanhuquqlari" />
+    </div>,
+    <div className="item" data-value="4">
+      <Image
+        src={medeniyyetveturizmnazirliyi}
+        alt="medeniyyetveturizmnazirliyi"
+      />
+    </div>,
+    <div className="item" data-value="5">
+      <Image src={ombudsman} alt="ombudsman" />
+    </div>,
+    <div className="item" data-value="6">
+      <Image src={sehersalma} alt="sehersalma" />
+    </div>,
+    <div className="item" data-value="7">
+      <Image src={sosialmudafienazirliyi} alt="sosialmudafienazirliyi" />
+    </div>,
+    <div className="item" data-value="8">
+      <Image src={undesa} alt="undesa" />
+    </div>,
+    <div className="item" data-value="9">
+      <Image src={undp} alt="undp" />
+    </div>,
   ];
 
   return (
     <div id="colleaguesComponent">
       <div className="container carousel">
         <h1>ƏMƏKDAŞLARIMIZ</h1>
-        <AliceCarousel
-          mouseTracking
-          items={items}
-          responsive={responsive}
-          controlsStrategy="alternate"
-        />
+        <AliceCarousel mouseTracking items={items} responsive={responsive} />
       </div>
     </div>
   );
