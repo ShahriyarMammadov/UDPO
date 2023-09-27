baf938444863c99198f70042e2af496201510562
elmancode <elmanaskarov@gmail.com>
2023-09-27 10:37:37 +0400

    update

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React from "react";
+import "./index.scss";
+import logo from "../../assets/logowhite.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="footer">
+        <div className="footer_top">
+          <nav className="container">
+            <div className="logo_menu">
+              <Link>
+                <img src={logo} alt="UDPO" />
+              </Link>
 
-export default Footer
+              <div className="menu">
+                <ul className="menu_row">
+                  <li>
+                    <Link to="/about"> Haqqımızda</Link>
+                  </li>
+                  <li>
+                    <Link to="/services"> Xidmətlər</Link>
+                  </li>
+                  <li>
+                    <Link to="/disability"> Əlillik</Link>
+                  </li>
+                </ul>
+                <ul className="menu_row">
+                  <li>
+                    <Link to="/news"> Xəbərlər</Link>
+                  </li>
+                  <li>
+                    <Link to="/galary"> Qalereya</Link>
+                  </li>
+                  <li>
+                    <Link to="/NGO">QHT </Link>
+                  </li>
+                </ul>
+                <ul className="menu_row">
+                  <li>
+                    <Link to="/partners"> Tərəfdaşlar</Link>
+                  </li>
+                  <li>
+                    <Link to="/contests"> Müsabiqələr</Link>
+                  </li>
+                  <li>
+                    <Link to="/competitions"> Müsahibələr</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="contact_social">
+              <div className="contact">
+                <p>
+                  Ünvan: AZ1138, Bakı şəhəri, Yasamal rayonu, <br /> Əhməd Cavad
+                  küçəsi 7a
+                </p>
+                <p>
+                  Telefon: (+994 12) 434 48 36; <br /> Mobil: (+994 50) 371 03
+                  02
+                </p>
+                <p>Tel/Faks: (+994 12) 434 93 29</p>
+                <p>Email: union-dpo@baku.az ; davudrehimli@gmail.com</p>
+              </div>
+
+              <div className="social_bar">
+                <ul>
+                  <li>
+                    <i class="fa-brands fa-facebook-f"></i>
+                  </li>
+                  <li>
+                    <i class="fa-brands fa-instagram"></i>
+                  </li>
+                  <li>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                  </li>
+                  <li>
+                    <i class="fa-brands fa-youtube"></i>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+            
+          </nav>
+        </div>
+
+        <div className="footer_bottom">
+          <p> © 2023 Bütün hüquqlar qorunur</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
