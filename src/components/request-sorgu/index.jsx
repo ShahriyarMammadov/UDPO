debfb21d4ec396c7b5f67d22af5a720e7fc0f641
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2024-02-18 12:51:07 +0400

    sorgu api connect

--- src/components/request-sorgu/index.jsx
+++ src/components/request-sorgu/index.jsx
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 import axios from "axios";
+import { Button } from "antd";
+import { SendOutlined } from "@ant-design/icons";
 
 const RequestComponent = () => {
-  const [radioValue, setRadioValue] = useState("");
+  const [radioValue, setRadioValue] = useState(90);
+  const [sorgu, setSorgu] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://udpobackend-production.up.railway.app/sorgu/getSorgu`
+      );
+
+      setSorgu(data?.data);
+    } catch (error) {
+      console.log(error, error?.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const sendRequest = async () => {
     try {
-      const { data } = await axios.post(`asdasdsdnfkjsa`, {
-        radioValue,
-      });
-      console.log(data);
+      setLoading(true);
+      const { data } = await axios.post(
+        `https://udpobackend-production.up.railway.app/sorgu/sorgular/${sorgu?._id}`,
+        {
+          optionIndex: radioValue,
+        }
+      );
+      // console.log(data);
+      setLoading(false);
     } catch (error) {
-      console.log(error);
+      console.log(error, error?.response?.data);
+      setLoading(false);
     }
   };
 
@@ -20,74 +46,42 @@ const RequestComponent = () => {
     <div id="request">
       <div className="container request">
         <h1>SORĞU</h1>
-        <h4>
-          Mövzu: ƏLİLLİYİ OLAN İNSANLARIN CƏMİYYƏTƏ İNTEQRASİYASINA NƏ MANE OLUR
-        </h4>
+        <h4>Mövzu: {sorgu?.name}</h4>
 
         <hr />
         <form
           action=""
-          onChange={(e) => {
-            setRadioValue(e.target.value);
-          }}
+          // onChange={(e) => {
+          //   setRadioValue();
+          // }}
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
-          <div>
-            <input
-              type="radio"
-              id="one"
-              name="drone"
-              value="Qanunların zəif icra olunması"
-            />
-            <label htmlFor="one">Qanunların zəif icra olunması</label>
-          </div>
+          {sorgu?.options?.map((e, i) => {
+            return (
+              <div key={e?._id}>
+                <input
+                  type="radio"
+                  id={e?._id}
+                  name="optionsGroup"
+                  value={e?.option}
+                  onChange={() => setRadioValue(i)}
+                />
+                <label htmlFor={e?._id}>{e?.option}</label>
+              </div>
+            );
+          })}
 
-          <div>
-            <input
-              type="radio"
-              id="two"
-              name="drone"
-              value="İnfrastrukturun uyğun olmaması"
-            />
-            <label htmlFor="two">İnfrastrukturun uyğun olmaması</label>
-          </div>
-
-          <div>
-            <input
-              type="radio"
-              id="four"
-              name="drone"
-              value="Əlilliyi olan insanların hüquqlarını bilməməsi "
-            />
-            <label htmlFor="four">
-              Əlilliyi olan insanların hüquqlarını bilməməsi{" "}
-            </label>
-          </div>
-
-          <div>
-            <input
-              type="radio"
-              id="five"
-              name="drone"
-              value="İctimai nəzarətin zəif olması"
-            />
-            <label htmlFor="five">İctimai nəzarətin zəif olması</label>
-          </div>
-
-          <div>
-            <input
-              type="radio"
-              id="six"
-              name="drone"
-              value="Laqeydlilik və məsuliyyətsizlik"
-            />
-            <label htmlFor="six">Laqeydlilik və məsuliyyətsizlik</label>
-          </div>
-          <button className="submitBtn" onClick={sendRequest}>
+          <Button
+            type="primary"
+            icon={<SendOutlined />}
+            loading={loading}
+            onClick={sendRequest}
+            disabled={sorgu?.result?.length > 0 || radioValue === 90}
+          >
             Göndər
-          </button>
+          </Button>
         </form>
       </div>
     </div>
